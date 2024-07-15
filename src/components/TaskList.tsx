f4e5952b68c34f89cd978d24eef8daaaa89a3c6b
Jonathan <designer.azrael@gmail.com>
2024-07-15 12:39:28 -0300

    ajustes finais

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -10,8 +10,7 @@ export function TaskList() {
   const sortedTasks = [...tasks].sort((a, b) => Number(b.completed) - Number(a.completed));
 
   return (
-    <>
-    <div id='list' className="bg-greenLogo w-full h-screen lg:h-[680px] lg:w-[500px] lg:rounded-2xl p-4 mr-4 justify-start items-center flex flex-col">
+    <div className="bg-greenLogo w-full h-screen lg:h-[680px] lg:w-[500px] lg:rounded-2xl p-4 mr-4 justify-start items-center flex flex-col">
       <h1 className="text-2xl font-semibold text-white">Lista de Tarefas</h1>
       <div className='flex justify-between w-full text-white font-semibold text-sm pb-6 pt-8'>
         <h1>Tarefas</h1>
@@ -66,6 +65,5 @@ export function TaskList() {
         <p className="text-white">Nenhuma tarefa encontrada.</p>
       )}
     </div>
-    </>
   );
 }
